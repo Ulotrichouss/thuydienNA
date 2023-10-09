f1d7db9fe02e962d78cc2cbccefe7966cb1e3766
Ulotrichouss <binprovtp621@gmail.com>
2023-10-09 23:15:03 +0700

    commmit index

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const multer = require("multer");
 const Tinhtoan = require("./models/tinhtoan.js");
 const Buocxa = require("./models/buocxa.js");
 const Hochua = require("./models/hochua.js");
 const reader = require("xlsx");
-const { ObjectId } = require("mongodb");
-const tinhtoan = require("./models/tinhtoan.js");
 require("dotenv").config();
 
 const app = express();
@@ -24,7 +21,7 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 //--------------
@@ -39,28 +36,28 @@ app.post("/tinhtoan/create", async (req, res) => {
     qho: req.body.qlake,
     timeday: req.body.timeday,
     timehour: req.body.timehour,
-    });
+  });
   data.save();
   res.redirect("/");
 });
 
-app.get("/", async (req, res, next) => {
+app.get("/", async (req, res) => {
   const data = await Tinhtoan.find({});
   res.render("table_tinhtoan", { table: data });
 });
 
 app.get("/add", async (req, res) => {
-  const data = await Hochua.find({})
-  res.render("form_add",{hochua:data});
+  const data = await Hochua.find({});
+  res.render("form_add", { hochua: data });
 });
 
-app.get("/:id", async (req, res) => {
-  const data = await Tinhtoan.findById({_id:req.params.id})
-  res.render("form_edit",{data:data});
+app.get("/edit/:id", async (req, res) => {
+  const data = await Tinhtoan.findById({ _id: req.params.id });
+  res.render("form_edit", { data: data });
 });
 
 app.post("/update/:id", async (req, res) => {
-  const data = await Tinhtoan.findByIdAndUpdate(req.params.id,{
+  const data = await Tinhtoan.findByIdAndUpdate(req.params.id, {
     nuoctruoc: req.body.wbefore,
     dungtichtruoc: req.body.capbefore,
     nuocsau: req.body.wafter,
@@ -70,13 +67,13 @@ app.post("/update/:id", async (req, res) => {
     qho: req.body.qlake,
     timeday: req.body.timeday,
     timehour: req.body.timehour,
-  })
-  data.save()
-  res.redirect("/")
-})
-app.get('/delete/:id', async (req, res) => {
-  const del = await Tinhtoan.findByIdAndDelete(req.params.id)
-  res.redirect("/")
+  });
+  data.save();
+  res.redirect("/");
+});
+app.get("/delete/:id", async (req, res) => {
+  const del = await Tinhtoan.findByIdAndDelete(req.params.id);
+  res.redirect("/");
 });
 //--------------
 app.get("/buocxa", async (req, res, next) => {
@@ -90,19 +87,23 @@ app.post("/buocxa/add", async (req, res) => {
 });
 //--------------
 
-app.get("/get", async (req, res) => {
-  const file = reader.readFile("hochua.xlsx");
-  let data = [];
-  const sheets = file.SheetNames;
+app.get("/getFile", async (req, res) => {
+  try {
+    const file = await reader.readFile("hochua.xlsx");
+    let data = [];
+    const sheets = file.SheetNames;
 
-  for (let i = 0; i < sheets.length; i++) {
-    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
-    temp.forEach((res) => {
-      data.push(res);
-    });
+    for (let i = 0; i < sheets.length; i++) {
+      const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
+      temp.forEach((res) => {
+        data.push(res);
+      });
+    }
+
+    res.json(data);
+  } catch (err) {
+    res.json(err);
   }
-
-  res.json(data);
 });
 
 app.get("/hochua", async (req, res) => {
@@ -111,20 +112,32 @@ app.get("/hochua", async (req, res) => {
 });
 
 app.post("/click", async (req, res) => {
-    const data = await Hochua.find({mnh:req?.body?.value})
+  if(Number.isInteger(Number(req?.body?.value))) {
+    const value = Number(req?.body?.value).toFixed(2).toString()
+    const data = await Hochua.find({ mnh: value });
     const obj = data.reduce((accumulator, data) => {
-        return {...accumulator, 'val': data.dungtich};
-      }, {});
+      return { ...accumulator, val: data.dungtich };
+    }, {});
 
-    return res.status(200).json({
-        data: obj
-      });
-})
+    return res.json({
+      data: obj,
+    });
+  } else {
+    const data = await Hochua.find({ mnh: req?.body?.value });
+    const obj = data.reduce((accumulator, data) => {
+      return { ...accumulator, val: data.dungtich };
+    }, {});
+
+    return res.json({
+      data: obj,
+    });
+  }
+});
 
 app.post("/hochua/add", async (req, res) => {
-    await Hochua.create(req.body)
-    res.json("success");
-  });
+  await Hochua.create(req.body);
+  res.json("success");
+});
 
 mongoose
   .connect(process.env.DB_URL, {
